88a3447872cfaca5e494e26e07b7a82bdc8ca78a
dcheplic <chepdj22@wclive.wesminster.edu>
2019-01-18 14:32:25 -0500

    now is in full screen w no way to get out (good)

--- src/com/palesd/gui/main/MainMenu.java
+++ src/com/palesd/gui/main/MainMenu.java
@@ -12,6 +12,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -36,6 +37,8 @@ public class MainMenu extends Application {
         bigStage = stage;
         bigStage.setScene(sceneStack.peek());
         bigStage.initStyle(StageStyle.UNDECORATED);
+        bigStage.setFullScreen(true);
+        bigStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         bigStage.show();
     }
 
